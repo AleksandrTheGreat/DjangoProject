a488e7181271f840e322bc1ef73562056821b866
AleksandrTheGreat <267179@bk.ru>
2024-02-07 02:51:47 +0500

    Начало верстки и настройка base.html в HP_World

--- HarryPotter/HP_World/urls.py
+++ HarryPotter/HP_World/urls.py
@@ -3,5 +3,10 @@ from . import views
 # import HP_World.views as views
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
+    path('foundation/<str:page>/', views.foundation, name='foundation'),
+    path('elements/<str:section>/', views.elements, name='elements'),
+    path('info/<str:id>/', views.info, name='info'),
+    path('grimoire/', views.grimoire, name='grimoire'),
+    path('shop/', views.shop, name='shop')
 ]
--- HarryPotter/HP_World/views.py
+++ HarryPotter/HP_World/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+global pages
+pages = ["Главная", "Книги", "Фильмы", "Игры", "Энциклопедия", "Магазин", "Контакты"]
 
 def main(request):
-    return render(request, template_name='HP_World/main.html')
+    data = {'pages': pages}
+    return render(request, template_name='HP_World/main.html', context=data)
+
+def foundation(request, page):
+    data = {'pages': pages, 'page':page}
+    print(page)
+    return render(request, template_name='HP_World/foundation.html', context=data)
+
+def elements(request, section):
+    data = {'pages': pages, 'section':section}
+    return render(request, template_name='HP_World/main.html', context=data)
+
+def info(request, id):
+    data = {'pages': pages, 'id':id}
+    return render(request, template_name='HP_World/main.html', context=data)
+
+def grimoire(request):
+    return HttpResponse('Grimoire')
+
+def shop(request):
+    return HttpResponse('Shop')
