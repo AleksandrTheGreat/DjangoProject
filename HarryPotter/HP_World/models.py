973f8d4f587b1dad99837328101c09d53e71d246
AleksandrTheGreat <267179@bk.ru>
2024-03-09 16:49:43 +0500

    Начал делать модели

--- HarryPotter/HP_World/models.py
+++ HarryPotter/HP_World/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50, default='Нет названия') # Название
+    description = models.TextField(default='Нет описания') # Описание
+    year = models.IntegerField(default='Нет даты') # Год выпуска
+    quote = models.CharField(max_length=200, default='Нет цитаты') # Цитата
+    cover = models.ImageField(upload_to='img/Books/')
